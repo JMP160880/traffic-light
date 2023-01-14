68639dd58812a93cd553f55f6637a9c793ff7e88
JMP160880 <javiermoranpando@gmail.com>
2023-01-14 22:37:42 +0000

    falta el formato

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,47 +2,50 @@ import React,{useState} from "react";
 
 const Home = () => {
 
-	const [red,setRed]= useState("opacity-25");
-	const [yellow,setyellow]= useState("opacity-25");
-	const [green,setGreen]= useState("opacity-25");
+	const [opacityRed,setOpacityRed]= useState("opacity-25");
+	const [opacityYellow,setOpacityYellow]= useState("opacity-25");
+	const [opacityGreen,setOpacityGreen]= useState("opacity-25");
 
-	
-	
+
 	const colorRed = () =>{
 		if(opacityRed === "opacity-25"){
-			setRed("opacity-100")
-			setYellow("opacity-25")
-			setGreen("opacity-25")
+			setOpacityRed("opacity-100")
+			setOpacityYellow("opacity-25")
+			setOpacityGreen("opacity-25")
 		}else{
-			setRed("opacity-25")
-		};
-	const colorYellow = () =>{
-		if(opacityYellow === "opacity-25"){
-			setYellow("opacity-100")
-			setRed("opacity-25")
-			setGreen("opacity-25")
-		}else{
-				setYellow("opacity-25")
-			}
-	const colorYellow = () =>{
-		if(opacityYellow === "opacity-25"){
-				setYellow("opacity-100")
-				setRed("opacity-25")
-				setGreen("opacity-25")
-	}else{
-				setYellow("opacity-25")
-}
-
+			setOpacityRed("opacity-25")
+		}
 	}
+
+	const colorYellow = () =>{
+		if(opacityYellow === "opacity-25"){
+			setOpacityYellow("opacity-100")
+			setOpacityRed("opacity-25")
+			setOpacityGreen("opacity-25")
+		}else{
+			setOpacityYellow("opacity-25")
+		}
+	}
+
+	const colorGreen = () =>{
+		if(opacityGreen === "opacity-25"){
+			setOpacityGreen("opacity-100")
+			setOpacityYellow("opacity-25")
+			setOpacityRed("opacity-25")
+		}else{
+			setOpacityGreen("opacity-25")
+		}
+	}
+
 
 	return (
 		<div className="semaforo">
-			<div className={"btn btn-danger " + red}  onClick= {colorRed} >uno</div>
-			<div className={"btn btn-warning "  + yellow} onClick= {colorYellow}>dos</div>
-			<div className={"btn btn-success "  + green} onClick= {colorGreen}>tres</div>
+			<div className={"btn btn-danger " + opacityRed}  onClick= {colorRed} ></div>
+			<div className={"btn btn-warning " + opacityYellow} onClick= {colorYellow}></div>
+			<div className={"btn btn-success " + opacityGreen} onClick= {colorGreen}></div>
 		</div>
 	);
 };
-}
+
 
 export default Home;
