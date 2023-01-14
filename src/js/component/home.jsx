48e507dfce7034723198b6e1edd44e869e468b3f
JMP160880 <javiermoranpando@gmail.com>
2023-01-14 16:15:11 +0000

    luego sigo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,26 +2,47 @@ import React,{useState} from "react";
 
 const Home = () => {
 
-	const [activo,setActivo]= useState("opacity-25");
-	
-	const changeColor = () =>{
-		
+	const [red,setRed]= useState("opacity-25");
+	const [yellow,setyellow]= useState("opacity-25");
+	const [green,setGreen]= useState("opacity-25");
 
-		if(activo == "opacity-25"){
-			setActivo("opacity-100");
-		}else {
-			setActivo("opacity-25");
-		}
+	
+	
+	const colorRed = () =>{
+		if(opacityRed === "opacity-25"){
+			setRed("opacity-100")
+			setYellow("opacity-25")
+			setGreen("opacity-25")
+		}else{
+			setRed("opacity-25")
+		};
+	const colorYellow = () =>{
+		if(opacityYellow === "opacity-25"){
+			setYellow("opacity-100")
+			setRed("opacity-25")
+			setGreen("opacity-25")
+		}else{
+				setYellow("opacity-25")
+			}
+	const colorYellow = () =>{
+		if(opacityYellow === "opacity-25"){
+				setYellow("opacity-100")
+				setRed("opacity-25")
+				setGreen("opacity-25")
+	}else{
+				setYellow("opacity-25")
+}
+
 	}
 
 	return (
 		<div className="semaforo">
-			<div className={"btn btn-danger " + activo} onClick= {changeColor} >uno</div>
-			<div className={"btn btn-warning "  + activo} onClick= {changeColor}>dos</div>
-			<div className={"btn btn-success "  + activo} onClick= {changeColor}>tres</div>
+			<div className={"btn btn-danger " + red}  onClick= {colorRed} >uno</div>
+			<div className={"btn btn-warning "  + yellow} onClick= {colorYellow}>dos</div>
+			<div className={"btn btn-success "  + green} onClick= {colorGreen}>tres</div>
 		</div>
 	);
 };
-
+}
 
 export default Home;
